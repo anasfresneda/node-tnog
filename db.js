e628fdd3802c43bea0b052eff94bbf75bcbf5b77
Sara Pinilla <sarapini19@gmail.com>
2019-03-26 15:20:57 +0100

    DB, node actualizado

---
+++ db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+
+let pool = null
+
+let connect = (done) => {
+    pool = mysql.createPool({
+        host: '127.0.0.1',
+        user:'root',
+        password:'root',
+        port: 8889,
+        database: 'thenogallery3',
+        multipleStatements: true
+    })
+    done()
+}
+
+let get = () => {
+    return pool
+}
+
+module.exports = {
+    connect: connect,
+    get: get
+}
